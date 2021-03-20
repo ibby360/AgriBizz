ec1ac5ba576a911fab2c0c9cae6c4284d93a06bb
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-20 20:45:42 +0300

    Add comments form

--- blog/forms.py
+++ blog/forms.py
@@ -1,19 +1,29 @@
-from django import forms 
-from tinymce import TinyMCE 
-from .models import _your_model_ 
+from django import forms
+from django.db.models import fields
+from django.db.models.base import Model
+from tinymce.widgets import TinyMCE
+from .models import _your_model_
+from blog.models import Comment
 
 
-class TinyMCEWidget(TinyMCE): 
-	def use_required_attribute(self, *args): 
-		return False
+class TinyMCEWidget(TinyMCE):
+    def use_required_attribute(self, *args):
+        return False
 
 
-class PostForm(forms.ModelForm): 
-	content = forms.CharField( 
-		widget=TinyMCEWidget( 
-			attrs={'required': False, 'cols': 50, 'rows': 30} 
-		) 
-	) 
-	class Meta: 
-		model = _your_model_ 
-		fields = '__all__'
+class PostForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=TinyMCEWidget(
+            attrs={'required': False, 'cols': 50, 'rows': 30}
+        )
+    )
+
+    class Meta:
+        model = _your_model_
+        fields = '__all__'
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body',)
