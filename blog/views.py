2b6e6d51fe866f6acdb3482eaed836c3993499c9
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-21 18:11:37 +0300

    Remove comment section

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # from django.http.response import Http404
 from django.shortcuts import render
 from blog.models import BlogPost, News
-# from .forms import CommentForm
 
 # Create your views here.
 
@@ -34,26 +33,8 @@ def farming_practice(request):
 
 def practice_details(request, slug):
     post = BlogPost.objects.get(slug=slug)
-    # comments = BlogPost.comments.filter(active=True)
-    # new_comment = None
-    # # comment posted
-    # if request.method == 'POST':
-    #     comment_form = CommentForm(data=request.POST)
-    #     if comment_form.is_valid():
-    #         # create comment object but don't save to database yet
-    #         new_comment = comment_form.save(commit=False)
-    #         # Assign the current post to the comment
-    #         new_comment.post = post
-    #         # Save the comment to the database
-    #         new_comment.save()
-
-    #     else:
-    #         comment_form = CommentForm()
     context = {
         'post': post,
-        # 'comments': comments,
-        # 'new_comment': new_comment,
-        # 'comment_form': comment_form,
     }
     return render(request, "practice_details.html", context)
 
