73e2c5600304b11d8bb82814afe814845377b17c
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-21 18:13:04 +0300

    Add contact model

--- mainApp/models.py
+++ mainApp/models.py
@@ -11,3 +11,11 @@ class Newsletter(models.Model):
     def __str__(self):
         return self.email
     
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
