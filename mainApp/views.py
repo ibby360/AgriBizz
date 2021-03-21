581ab13fa3f630a7462467dee5e4e80d8c350a37
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-21 18:14:01 +0300

    Add contact view and it's functions

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
+from django.core.checks import messages
 from django.shortcuts import render
-from mainApp.models import Newsletter
+from mainApp.models import Newsletter, Contact
 from blog.models import BlogPost, News
 from blog.views import practice_details
 
@@ -25,4 +27,19 @@ def index(request):
     return render(request, 'index.html', context)
 
 def contact(request):
+    if request.method == 'POST':
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        contact.name = name
+        contact.email = email
+        contact.subject = subject
+        contact.message = message
+        contact.save()
+        return HttpResponse('<h1> THANKS FOR CONTACT US </H1>')
+        
+
     return render(request, 'contact.html',)
