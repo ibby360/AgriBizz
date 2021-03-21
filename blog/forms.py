229c982cb6a612b3872a49cbd5add14b06329ff0
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-21 18:10:37 +0300

    Remove comment form

--- blog/forms.py
+++ blog/forms.py
@@ -3,7 +3,6 @@ from django.db.models import fields
 from django.db.models.base import Model
 from tinymce.widgets import TinyMCE
 from .models import _your_model_
-from blog.models import Comment
 
 
 class TinyMCEWidget(TinyMCE):
@@ -23,7 +22,3 @@ class PostForm(forms.ModelForm):
         fields = '__all__'
 
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ('name', 'email', 'body',)
