b18f18ffec3dd1ce23a587370241b5d0ca6c9843
Ibby360 <ibrahimramadhan360@outlook.com>
2021-03-21 18:12:28 +0300

    Register contact

--- mainApp/admin.py
+++ mainApp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from mainApp.models import Newsletter
+from mainApp.models import Newsletter, Contact
 # Register your models here.
 admin.site.register(Newsletter)
+admin.site.register(Contact)
+
