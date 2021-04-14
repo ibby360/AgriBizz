28941f6d5179499032bc5bd111e29c81bf05c496
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-14 12:32:48 +0300

    add envireoment variables

--- agribizz/settings.py
+++ agribizz/settings.py
@@ -14,6 +14,14 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 
+import environ
+
+#################################
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+#################################
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^0-le@s1wj(afm#9wyhq(#c#pw^g5$!h861+5!0zexuj9eg)q2'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
