0b48d419ead4957d36e5a5f62134bfe5893fe8d7
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-18 13:43:23 +0300

    some changes

--- eccomerce/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-import eccomerce.models as eccom
-# Register your models here.
-admin.site.register(eccom.Customer)
-admin.site.register(eccom.Product)
-admin.site.register(eccom.Order)
-admin.site.register(eccom.OrderItem)
-admin.site.register(eccom.ShippingAddress)
-#admin.site.register(app_model.Post)
