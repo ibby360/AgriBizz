848dee82837c0c05dc5f047dff7500d1785ec849
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-19 14:09:08 +0300

    Feat: featured product view

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from blog.models import BlogPost
 
 
 # Create your views here.
 def products(request):
+    posts = BlogPost.objects.order_by('-date_created')[:3]
     product_list = Product.objects.all()
     paginator = Paginator(product_list, 6)
     page_request_var = 'page'
@@ -18,13 +20,18 @@ def products(request):
     context = {
         'queryset': paginated_queryset,
         'page_request_var': page_request_var,
+        'posts': posts,
+
     }    
     return render(request, 'products/products.html', context)
+    
 
 def product_details(request, slug):
     product = Product.objects.get(slug = slug)
+    related = Product.objects.order_by('-date_created')[:4]
     context = {
         'item': product,
+        'related': related,
     }
         
     return render(request, 'products/product_details.html', context)
