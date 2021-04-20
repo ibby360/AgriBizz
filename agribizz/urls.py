c34f2ff6ec415102a70a7cfbe2883eff140966e8
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-20 05:48:47 +0300

    Refactor: group related app urls

--- agribizz/urls.py
+++ agribizz/urls.py
@@ -16,20 +16,24 @@ from blog.views import (farm_management, farming_practice,
 handler404 = views.error_404
 
 urlpatterns = [
+    # Core
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
 
+    # mainApp
     path('', views.index, name='index'),
+    path('search', views.search_page, name='search'),
+    path('contact', views.contact, name="contact-view"),
+    path('about', views.about_us, name='about'),
+    path('thank_you', views.thank_you, name='thanks-page'),
+
+    # Blog
     path('farmimng_practice', farming_practice, name='farming_practice'),
     path('farming_practice/<slug:slug>', practice_details, name='practice_details'),
-    path('contact', views.contact, name="contact-view"),
     path('news', news_view, name='news-view'),
-    path('thank_you', views.thank_you, name='thanks-page'),
     path('practice_intro', practice_intro, name='practice_intro'),
     path('news/<slug:slug>', news_details, name="news_details"),
-    path('about', views.about_us, name='about'),
     path('farm_management', farm_management, name='farm_management'),
-    path('search', views.search_page, name='search'),
 
     # Products 
     path('products', products, name='products'),
