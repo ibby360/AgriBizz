1143162c897628b9b5690a6690f3636be38c4f3e
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-26 23:50:29 +0300

    Create post_product model

--- marketing/models.py
+++ marketing/models.py
@@ -4,7 +4,60 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
-class Person(models.Model):
+# class Person(models.Model):
+#     # Region choices
+#     CHOICES = (
+#         ('Arusha', 'Arusha'),
+#         ('Dar es Salaam', 'Dar es Salaam'),
+#         ('Geita', 'Geita'),
+#         ('Kagera', 'Kagera'),
+#         ('Mwanza', 'Mwanza'),
+#         ('Morogoro', 'Morogoro'),
+#         ('Shinyanga', 'Shinyanga'),
+#         ('Tabora', 'Tabora'),
+#         ('Singida', 'Singida'),
+#         ('Simiyu', 'Simiyu'),
+#         ('Mbeya', 'Mbeya'),
+#     )
+#     full_name = models.CharField(max_length=50, null=False, blank=False)
+#     email = models.EmailField(max_length=254, )
+#     phone_number = PhoneNumberField(null=False, blank=False)
+#     region = models.CharField(max_length=50, choices=CHOICES) 
+#     district = models.CharField(max_length=50)
+#     ward = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return self.full_name
+
+
+# class Product(models.Model):
+#     PRODUCT_CHOICES = (
+#         ('Maize', 'Maize'),
+#         ('Rice', 'Rice'),
+#         ('Beans', 'Beans'),
+#         ('Carrot', 'Carrot'),
+#         ('Cabage', 'Cabage'),
+#         ('Beans', 'Beans'),
+#     )
+    
+#     SCALE_CHOICES = (
+#         ('KG', 'Kg'),
+#         ('DEBE', 'Debe'),
+#         ('GUNIA', 'Gunia'),
+#         ('TANI', 'Tani'),
+#     )
+#     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     product_name = models.CharField(max_length=100, choices= PRODUCT_CHOICES)
+#     scale = models.CharField(max_length=50, choices=SCALE_CHOICES, default='Kg')
+#     price = models.FloatField(max_length=100, null=False, blank=False)
+#     date_created = models.DateTimeField(auto_now_add = True)
+#     thumbnail = ImageField(upload_to='static/img/', null=True, blank=True)
+
+#     def __str__(self):
+#         return self.product_name
+
+
+class PostProduct(models.Model):
     # Region choices
     CHOICES = (
         ('Arusha', 'Arusha'),
@@ -19,24 +72,43 @@ class Person(models.Model):
         ('Simiyu', 'Simiyu'),
         ('Mbeya', 'Mbeya'),
     )
+
+    # Product Choices
+    PRODUCT_CHOICES = (
+        ('Maize', 'Maize'),
+        ('Rice', 'Rice'),
+        ('Beans', 'Beans'),
+        ('Carrot', 'Carrot'),
+        ('Cabage', 'Cabage'),
+        ('Beans', 'Beans'),
+    )
+
+    # Scale Choice
+    SCALE_CHOICES = (
+        ('Kg', 'Kg'),
+        ('Debe', 'Debe'),
+        ('Sack', 'Sack'),
+        ('Tons', 'Tons'),
+    )
+
+    # Person's Details
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, )
     phone_number = PhoneNumberField(null=False, blank=False)
     region = models.CharField(max_length=50, choices=CHOICES) 
-    district = models.CharField(max_length=50)
-    ward = models.CharField(max_length=50)
+    location = models.CharField(max_length=150, default='')
 
     def __str__(self):
-        return self.first_name
+        return self.full_name
 
-
-class Product(models.Model):
-    owner = models.ForeignKey(Person, on_delete=models.CASCADE)
-    product_name = models.CharField(max_length=100)
-    quantity = models.CharField(max_length=150)
+    # Product's Detials
+    product_name = models.CharField(max_length=100, choices= PRODUCT_CHOICES)
+    scale = models.CharField(max_length=50, choices=SCALE_CHOICES, default='Kg')
+    amount = models.IntegerField(null=False, blank=False, default='00')
     price = models.FloatField(max_length=100, null=False, blank=False)
     date_created = models.DateTimeField(auto_now_add = True)
     thumbnail = ImageField(upload_to='static/img/', null=True, blank=True)
 
     def __str__(self):
         return self.product_name
+        
