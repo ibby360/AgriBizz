735ab33d15dfe9f22577bedf5026285ab0f1e29c
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-27 15:52:15 +0300

    Refactor: Remove junk code

--- marketing/models.py
+++ marketing/models.py
@@ -1,60 +1,6 @@
 from django.db import models
+from django.db.models.fields import CharField, SmallIntegerField
 from django.db.models.fields.files import ImageField
-from phonenumber_field.modelfields import PhoneNumberField
-
-# Create your models here.
-
-# class Person(models.Model):
-#     # Region choices
-#     CHOICES = (
-#         ('Arusha', 'Arusha'),
-#         ('Dar es Salaam', 'Dar es Salaam'),
-#         ('Geita', 'Geita'),
-#         ('Kagera', 'Kagera'),
-#         ('Mwanza', 'Mwanza'),
-#         ('Morogoro', 'Morogoro'),
-#         ('Shinyanga', 'Shinyanga'),
-#         ('Tabora', 'Tabora'),
-#         ('Singida', 'Singida'),
-#         ('Simiyu', 'Simiyu'),
-#         ('Mbeya', 'Mbeya'),
-#     )
-#     full_name = models.CharField(max_length=50, null=False, blank=False)
-#     email = models.EmailField(max_length=254, )
-#     phone_number = PhoneNumberField(null=False, blank=False)
-#     region = models.CharField(max_length=50, choices=CHOICES) 
-#     district = models.CharField(max_length=50)
-#     ward = models.CharField(max_length=50)
-
-#     def __str__(self):
-#         return self.full_name
-
-
-# class Product(models.Model):
-#     PRODUCT_CHOICES = (
-#         ('Maize', 'Maize'),
-#         ('Rice', 'Rice'),
-#         ('Beans', 'Beans'),
-#         ('Carrot', 'Carrot'),
-#         ('Cabage', 'Cabage'),
-#         ('Beans', 'Beans'),
-#     )
-    
-#     SCALE_CHOICES = (
-#         ('KG', 'Kg'),
-#         ('DEBE', 'Debe'),
-#         ('GUNIA', 'Gunia'),
-#         ('TANI', 'Tani'),
-#     )
-#     owner = models.ForeignKey(Person, on_delete=models.CASCADE)
-#     product_name = models.CharField(max_length=100, choices= PRODUCT_CHOICES)
-#     scale = models.CharField(max_length=50, choices=SCALE_CHOICES, default='Kg')
-#     price = models.FloatField(max_length=100, null=False, blank=False)
-#     date_created = models.DateTimeField(auto_now_add = True)
-#     thumbnail = ImageField(upload_to='static/img/', null=True, blank=True)
-
-#     def __str__(self):
-#         return self.product_name
 
 
 class PostProduct(models.Model):
@@ -94,18 +40,18 @@ class PostProduct(models.Model):
     # Person's Details
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, )
-    phone_number = PhoneNumberField(null=False, blank=False)
-    region = models.CharField(max_length=50, choices=CHOICES) 
+    phone_number = models.CharField(max_length=13,null=False, blank=False)
+    region = models.CharField(max_length=50, choices=CHOICES,) 
     location = models.CharField(max_length=150, default='')
 
     def __str__(self):
         return self.full_name
 
     # Product's Detials
-    product_name = models.CharField(max_length=100, choices= PRODUCT_CHOICES)
-    scale = models.CharField(max_length=50, choices=SCALE_CHOICES, default='Kg')
-    amount = models.IntegerField(null=False, blank=False, default='00')
-    price = models.FloatField(max_length=100, null=False, blank=False)
+    product_name = models.CharField(max_length=100, choices= PRODUCT_CHOICES,)
+    scale = models.CharField(max_length=50, choices=SCALE_CHOICES,)
+    amount = models.IntegerField(null=False, blank=False, default='Enter Amount')
+    price = models.FloatField(max_length=100, null=False, blank=False, default='Enter Price')
     date_created = models.DateTimeField(auto_now_add = True)
     thumbnail = ImageField(upload_to='static/img/', null=True, blank=True)
 
