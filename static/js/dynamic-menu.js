754ff1c7d3623d734b98c8ff9e5e9f2fabebff77
Ibby360 <ibrahimramadhan360@outlook.com>
2021-04-29 00:43:47 +0300

    Feat: make  card-hover class change dynamic

--- static/js/dynamic-menu.js
+++ static/js/dynamic-menu.js
@@ -10,3 +10,13 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+$(document).ready(function() {
+	
+	$('.card').delay(1800).queue(function(next) {
+		$(this).removeClass('hover');
+		$('a.hover').removeClass('hover');
+		next();
+	});
+});
