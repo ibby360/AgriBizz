120898fb9b919837ab23a8b966031ce22a546d4c
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-13 16:00:35 +0300

    Remove duplicate imports

--- marketing/admin.py
+++ marketing/admin.py
@@ -1,5 +1,3 @@
-from marketing.views import post_product
-from products.views import products
 from django.contrib import admin
 from marketing.models import PostProduct
 # Register your models here.
