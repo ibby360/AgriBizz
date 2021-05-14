d0a0d6e0ff0b1ca33992a306cae535c5d63e8679
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-14 21:17:03 +0300

    Add district and ward field

--- marketing/models.py
+++ marketing/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.db.models.fields import CharField, SmallIntegerField
 from django.db.models.fields.files import ImageField
+from mtaa import tanzania
 
 
 class PostProduct(models.Model):
     # Region choices
+    
     CHOICES = (
         ('Arusha', 'Arusha'),
         ('Dar es Salaam', 'Dar es Salaam'),
@@ -17,6 +19,19 @@ class PostProduct(models.Model):
         ('Singida', 'Singida'),
         ('Simiyu', 'Simiyu'),
         ('Mbeya', 'Mbeya'),
+        ('Mara', 'Mara'),
+        ('Kilimanjaro', 'Kilimanjaro'),
+        ('Tanga', 'Tanga'),
+        ('Kigoma', 'Kigoma'),
+        ('Mtwara', 'Mtwara'),
+        ('Pwani', 'Pwani'),
+        ('Simiyu', 'Simiyu'),
+        ('Songwe', 'Songwe'),
+        ('Lindi', 'Lindi'),
+        ('Dodoma', 'Dodoma'),
+        ('Singida', 'Singida'),
+        ('Ruvuma', 'Ruvuma'),
+        ('Njombe', 'Njombe'),
     )
 
     # Product Choices
@@ -27,6 +42,14 @@ class PostProduct(models.Model):
         ('Carrot', 'Carrot'),
         ('Cabage', 'Cabage'),
         ('Beans', 'Beans'),
+        ('Vegetables', 'Vegetables'),
+        ('Ginger', 'Ginger'),
+        ('Garlic','Garlic'),
+        ('Banana', 'Banana'),
+        ('Tomato', 'Tomato'),
+        ('Lemon', 'Lemon'),
+        ('Coffee', 'Coffee'),
+
     )
 
     # Scale Choice
@@ -39,10 +62,12 @@ class PostProduct(models.Model):
 
     # Person's Details
     full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, )
+    email = models.EmailField(max_length=254,blank= True, null=True )
     phone_number = models.CharField(max_length=13,null=False, blank=False)
-    region = models.CharField(max_length=50, choices=CHOICES,) 
-    location = models.CharField(max_length=150, default='')
+    region = models.CharField(max_length=50, choices=CHOICES, ) 
+    district = models.CharField(max_length=150, default='')
+    ward = models.CharField(max_length=150, default='')
+
 
     def __str__(self):
         return self.full_name
@@ -53,7 +78,7 @@ class PostProduct(models.Model):
     amount = models.IntegerField(null=False, blank=False, default='Enter Amount')
     price = models.FloatField(max_length=100, null=False, blank=False, default='Enter Price')
     date_created = models.DateTimeField(auto_now_add = True)
-    thumbnail = ImageField(upload_to='static/img/', null=True, blank=True)
+    thumbnail = ImageField(upload_to='static/img/', default='No-Image-Placeholder.svg')
 
     def __str__(self):
         return self.product_name
