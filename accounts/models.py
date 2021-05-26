c994bee55b722214a3101324cdf75b914afe2946
Ibrahim Ramadhan <51038084+ibby360@users.noreply.github.com>
2021-05-26 14:37:41 +0300

    Update accounts/models.py
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,7 @@ class Customer(models.Model):
 	name = models.CharField(max_length=200, default='', blank=True)
 	phone = models.CharField(max_length=200, default='', blank=True)
 	email = models.CharField(max_length=200, default='', blank=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
 	def __str__(self):
 		return self.name
