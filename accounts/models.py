506dfaae34b84ab9b5efd8558a4e8d6b2aadb7cc
Ibrahim Ramadhan <51038084+ibby360@users.noreply.github.com>
2021-05-26 14:38:24 +0300

    Update accounts/models.py
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>

--- accounts/models.py
+++ accounts/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
 	)
 	date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 	order_status = models.CharField(max_length=200, null=True, choices=STATUS)
-	note = models.CharField(max_length=1000, null=True)
+	note = models.TextField(default='', blank=True)
 
 	def __str__(self):
 		return self.product.name
