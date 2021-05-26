1ea1beed7fae3e6dee7155356d6d21c3175ca1bf
Ibrahim Ramadhan <51038084+ibby360@users.noreply.github.com>
2021-05-26 14:37:23 +0300

    Update accounts/models.py
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,7 @@ from marketing.models import PostProduct
 class Customer(models.Model):
 	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, blank=True)
 	name = models.CharField(max_length=200, default='', blank=True)
-	phone = models.CharField(max_length=200, null=True)
+	phone = models.CharField(max_length=200, default='', blank=True)
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
