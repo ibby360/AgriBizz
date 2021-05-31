d3a5fc7671f45db0d16eecd14c189b5c1247fb52
Ibby360 <ibrahimramadhan360@outlook.com>
2021-05-31 17:28:03 +0300

    Create shop URLConf file

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from shop import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('product_list', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_details, name='product_details'),
+    path('checkout', views.checkout, name='checkout'),
+]
