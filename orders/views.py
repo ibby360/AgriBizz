07c130db56406ccadb0bfe227c8d16f41f755592
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-01 00:29:23 +0300

    Create order app

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
+from shop.models import Product
 from django.shortcuts import render
-
+from orders.models import OrderItem
+from orders.forms import OrderCreateForm
+from cart.cart import Cart
 # Create your views here.
+
+
+def checkout(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+
+            return render(request, 'orders/successful.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+
+    context = {
+        'cart': cart,
+        'form': form
+    }
+    return render(request, 'orders/checkout.html', context)
