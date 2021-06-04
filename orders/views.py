66a6528138eb9966357695173adb7282da61e0cb
Ibby360 <ibrahimramadhan360@outlook.com>
2021-06-04 17:05:40 +0300

    Launch asynchronous task

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
 from shop.models import Product
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from orders.models import OrderItem
 from orders.forms import OrderCreateForm
 from cart.cart import Cart
+from orders.tasks import order_created
 # Create your views here.
 
 
@@ -17,8 +19,14 @@ def checkout(request):
                     order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
             # clear the cart
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse('payment:process'))
 
-            return render(request, 'orders/successful.html', {'order': order})
+            # return render(request, 'orders/successful.html', {'order': order})
     else:
         form = OrderCreateForm()
 
